bb31f348047ea9602b7f76da8907bd4f770fd818
Davide Bellone <davide.bellone>
2020-09-14 10:57:09 +0200

    Add project files.

---
+++ DateTimeInjection/Controllers/WithDIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateTimeInjection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WithDIController : ControllerBase
+    {
+        private readonly IDateTimeService dateTimeService;
+
+        public WithDIController(IDateTimeService dateTimeService)
+        {
+            this.dateTimeService = dateTimeService;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return dateTimeService.Now().ToString();
+        }
+
+
+    }
+
+    public interface IDateTimeService
+    {
+        DateTime Now();
+    }
+
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime Now()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+
+}
---
+++ DateTimeInjection/Controllers/WithFunctionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateTimeInjection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WithFunctionController : ControllerBase
+    {
+        private readonly Func<DateTime> now;
+
+        public WithFunctionController(Func<DateTime> now = null)
+        {
+            if (now != null)
+            {
+                this.now = now;
+            }
+            else
+            {
+                this.now = new Func<DateTime>(() => DateTime.UtcNow);
+            }
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return this.now().ToString();
+        }
+    }
+}
